2ce00c2d20d074b4e7521d95de02b32606d0c00a
Umair Syed <itsumairsyed@gmail.com>
2023-03-29 10:17:32 +0500

    redux is configured. Store, selector and dipatch are used

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -1,3 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+
 export default function ShopsPage() {
-    return <h1>Welcome to Shops Page</h1>
+  const dispatch =  useDispatch()
+  const store = useSelector((store) => store);
+
+  function clickHandler() {
+    dispatch({ type: "CHANGE_APP_NAME", payload: "Umair's App"})
+  }
+
+  return (
+    <>
+      <h1>Welcome to {store.appName}</h1>
+      <button onClick={clickHandler}>Change app name</button>
+    </>
+  );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,18 @@
-import { createStore } from "react"
+import { createStore } from "redux";
+
+const initialState = {
+    isLoggedIn: false,
+    appName: "Zen React"
+}
+
+function reducerFunction(state, action) {
+    if(action.type === "CHANGE_APP_NAME") {
+        return {...state, appName: action.payload}
+    } else {
+        return state;
+    }
+}
+
+const store = createStore(reducerFunction, initialState);
+
+export default store;
